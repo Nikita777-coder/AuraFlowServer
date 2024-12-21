f303f02e08fd62e8ceb2180e6ada4c201d505ce5
Nikita777-coder <61533809+Nikita777-coder@users.noreply.github.com>
2024-12-21 22:21:18 +0300

    comment not realized code

--- main/src/main/java/app/controller/PremiumController.java
+++ main/src/main/java/app/controller/PremiumController.java
@@ -9,23 +9,23 @@ import java.util.UUID;
 @RequestMapping("/premium")
 @RequiredArgsConstructor
 public class PremiumController {
-    private final PremiumService premiumService;
-    @GetMapping
-    @ResponseBody
-    // Get current time in repository for resolving current premium
-    public Boolean hasPremium(@RequestParam("id") UUID userId) {
-        return premiumService.hasPremium(userId);
-    }
-
-    @PostMapping
-    @ResponseBody
-    public PremiumPaymentData buyPremium(@RequestBody UUID userId) {
-        return premiumService.buyPremium(userId);
-    }
-
-    @GetMapping("/history")
-    @ResponseBody
-    public List<PremiumData> getHistoryOfPremiums(@RequestParam("id") UUID userId) {
-        return premiumService.getHistoryOfPremiums(userId);
-    }
+//    private final PremiumService premiumService;
+//    @GetMapping
+//    @ResponseBody
+//    // Get current time in repository for resolving current premium
+//    public Boolean hasPremium(@RequestParam("id") UUID userId) {
+//        return premiumService.hasPremium(userId);
+//    }
+//
+//    @PostMapping
+//    @ResponseBody
+//    public PremiumPaymentData buyPremium(@RequestBody UUID userId) {
+//        return premiumService.buyPremium(userId);
+//    }
+//
+//    @GetMapping("/history")
+//    @ResponseBody
+//    public List<PremiumData> getHistoryOfPremiums(@RequestParam("id") UUID userId) {
+//        return premiumService.getHistoryOfPremiums(userId);
+//    }
 }
--- main/src/main/java/app/controller/StatisticController.java
+++ main/src/main/java/app/controller/StatisticController.java
@@ -10,20 +10,20 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/statistic")
 @RequiredArgsConstructor
 public class StatisticController {
-    private final StatisticService statisticService;
-    @PostMapping
-    @ResponseBody
-    public Statistic createUserStatistic(@Valid @RequestBody Statistic statistic) {
-        return statisticService.createNewUserStatistic(statistic);
-    }
-    @GetMapping
-    @ResponseBody
-    public Statistic getUserStatistic(@Valid @RequestBody Period timePeriod) {
-        return statisticService.getUserStatistic(timePeriod);
-    }
-    @PatchMapping
-    @ResponseBody
-    public Statistic updateUserStatistic(@Valid @RequestBody Statistic statistic) {
-        return statisticService.updateUserStatistic(statistic);
-    }
+//    private final StatisticService statisticService;
+//    @PostMapping
+//    @ResponseBody
+//    public Statistic createUserStatistic(@Valid @RequestBody Statistic statistic) {
+//        return statisticService.createNewUserStatistic(statistic);
+//    }
+//    @GetMapping
+//    @ResponseBody
+//    public Statistic getUserStatistic(@Valid @RequestBody Period timePeriod) {
+//        return statisticService.getUserStatistic(timePeriod);
+//    }
+//    @PatchMapping
+//    @ResponseBody
+//    public Statistic updateUserStatistic(@Valid @RequestBody Statistic statistic) {
+//        return statisticService.updateUserStatistic(statistic);
+//    }
 }
