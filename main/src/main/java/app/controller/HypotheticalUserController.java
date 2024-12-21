b63a753d3c254a3e48e637fa2d14e97920956918
Nikita777-coder <61533809+Nikita777-coder@users.noreply.github.com>
2024-12-21 16:54:07 +0300

    add controller of ficha

---
+++ main/src/main/java/app/controller/HypotheticalUserController.java
@@ -0,0 +1,12 @@
+package app.controller;
+
+/**
+ * A class for a hypothetical user so that they don't have to find
+ * out their preferences when registering, creating an account,
+ * and immediately give out the necessary meditations without wasting their time.
+ *
+ * Hypothetical user - user who don't registered in app (identified by device_id) and have limit functional
+ * (which functions wouldn't be in functional it's being clarified)
+ */
+public class HypotheticalUserController {
+}
