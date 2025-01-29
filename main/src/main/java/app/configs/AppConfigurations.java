0dbe3bfdc921bb4277911e5bb5169e6d7dc3cc42
Nikita777-coder <61533809+Nikita777-coder@users.noreply.github.com>
2025-01-29 21:48:18 +0300

    add regular app actions

--- main/src/main/java/app/configs/AppConfigurations.java
+++ main/src/main/java/app/configs/AppConfigurations.java
@@ -5,21 +5,33 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableScheduling
 public class AppConfigurations {
-    @Value("${services-configs.keycloak.url}")
+    @Value("${server.keycloak.url}")
     private String keyCloakUrl;
+
+    @Value("${server.web-client.time-response}")
+    private int responseTimeout;
 
     private final UserRepository userRepository;
 
@@ -55,9 +67,30 @@ public class AppConfigurations {
     }
 
     @Bean
+    public WebClient webClient() {
+        HttpClient httpClient = HttpClient.create()
+                .responseTimeout(Duration.ofSeconds(responseTimeout));
+
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .defaultStatusHandler(HttpStatusCode::is5xxServerError,
+                        (this::handleServerErrors))
+                .defaultStatusHandler(HttpStatusCode::is4xxClientError,
+                        (this::handleClientErrors))
+                .build();
+    }
+
+    @Bean
     public WebClient keyCloakWebClient() {
         return WebClient.builder()
                 .baseUrl(keyCloakUrl)
                 .build();
+    }
+
+    private Mono<? extends Throwable> handleServerErrors(ClientResponse clientResponse) {
+        throw new IllegalStateException(clientResponse.toString());
+    }
+    private Mono<? extends Throwable> handleClientErrors(ClientResponse clientResponse) {
+        throw new IllegalStateException(clientResponse.toString());
     }
 }
