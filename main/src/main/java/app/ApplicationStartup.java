8a652456152f1bf842bd0e650f4777dfe7e81104
Nikita777-coder <61533809+Nikita777-coder@users.noreply.github.com>
2025-03-19 00:55:11 +0300

    commit

--- main/src/main/java/app/ApplicationStartup.java
+++ main/src/main/java/app/ApplicationStartup.java
@@ -6,6 +6,7 @@ import app.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -14,18 +15,18 @@ import org.springframework.stereotype.Component;
 public class ApplicationStartup
         implements ApplicationListener<ApplicationReadyEvent> {
     private final UserRepository userRepository;
-    private final PasswordEncoder bCryptPasswordEncoder;
     /**
      * This event is executed as late as conceivably possible to indicate that
      * the application is ready to service requests.
      */
     @Override
     public void onApplicationEvent(final ApplicationReadyEvent event) {
+        PasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 
         userRepository.save(UserEntity
                 .builder()
                         .countBreathPractiseReminderPerDay(4)
-                        .email("admin1@a.com")
+                        .email("admin2@a.com")
                         .isPremium(true)
                         .name("name")
                         .role(Role.ADMIN)
