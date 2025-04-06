fc1e52de03ee6efb3ff93c922610b92afb82f4de
Nikita777-coder <61533809+Nikita777-coder@users.noreply.github.com>
2025-04-07 02:22:57 +1000

    add not tested meditation album part without user meditations

---
+++ main/src/main/java/app/mapper/MeditationAlbumMapper.java
@@ -0,0 +1,40 @@
+package app.mapper;
+
+import app.dto.meditationalbum.MeditationAlbum;
+import app.dto.meditationalbum.MeditationAlbumPlatform;
+import app.dto.meditationalbum.MeditationAlbumRequest;
+import app.entity.MeditationAlbumEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring",
+        uses = MeditationMapper.class)
+public interface MeditationAlbumMapper {
+    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "meditations", ignore = true)
+    MeditationAlbumEntity meditationAlbumRequestToMeditationAlbumEntity(MeditationAlbumRequest meditationAlbumRequest);
+    MeditationAlbum meditationAlbumEntityToMeditationAlbum(MeditationAlbumEntity entity);
+    MeditationAlbumPlatform meditationAlbumEntityToMeditationAlbumPlatform(MeditationAlbumEntity entity);
+    List<MeditationAlbumPlatform> meditationAlbumEntitiesToMeditationAlbumsPlatform(List<MeditationAlbumEntity> entities);
+    List<MeditationAlbum> meditationAlbumEntitiesToMeditationAlbums(List<MeditationAlbumEntity> entities);
+    @Mapping(target = "title", expression = "java(title(request, entity))")
+    @Mapping(target = "description", expression = "java(description(request, entity))")
+    @Mapping(target = "meditations", source = "request.meditations")
+    MeditationAlbumRequest prepareMeditationAlbumRequestFromOldMeditationAlbumEntity(MeditationAlbumRequest request, MeditationAlbumEntity entity);
+    default String title(MeditationAlbumRequest request, MeditationAlbumEntity entity) {
+        if (request.getTitle() == null) {
+            return entity.getTitle();
+        }
+
+        return request.getTitle();
+    }
+    default String description(MeditationAlbumRequest request, MeditationAlbumEntity entity) {
+        if (request.getDescription() == null) {
+            return entity.getDescription();
+        }
+
+        return request.getDescription();
+    }
+}
