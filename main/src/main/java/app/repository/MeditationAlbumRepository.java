6ab9eecbddeb72af5c78dd3395bbe23db3bffded
Nikita777-coder <61533809+Nikita777-coder@users.noreply.github.com>
2025-04-07 02:22:57 +1000

    add not tested meditation album part without user meditations

---
+++ main/src/main/java/app/repository/MeditationAlbumRepository.java
@@ -0,0 +1,13 @@
+package app.repository;
+
+import app.entity.MeditationAlbumEntity;
+import app.entity.userattributes.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface MeditationAlbumRepository extends JpaRepository<MeditationAlbumEntity, UUID> {
+    List<MeditationAlbumEntity> findAllByUser_Email(String email);
+    List<MeditationAlbumEntity> findAllByUser_Role(Role role);
+}
