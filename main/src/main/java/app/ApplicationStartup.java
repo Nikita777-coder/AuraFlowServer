a138138ba69e78b5116588571723d842a7e075ac
Nikita777-coder <61533809+Nikita777-coder@users.noreply.github.com>
2025-04-06 21:45:27 +1000

    update start params

--- main/src/main/java/app/ApplicationStartup.java
+++ main/src/main/java/app/ApplicationStartup.java
@@ -5,56 +5,40 @@ import app.entity.userattributes.Role;
 import app.repository.MeditationRepository;
 import app.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
 public class ApplicationStartup
         implements ApplicationListener<ApplicationReadyEvent> {
     private final UserRepository userRepository;
-    private final MeditationRepository meditationRepository;
     private final PasswordEncoder bCryptPasswordEncoder;
-    /**
-     * This event is executed as late as conceivably possible to indicate that
-     * the application is ready to service requests.
-     */
+
+    @Value("${server.admin.email}")
+    private String adminEmail;
+    @Value("${server.admin.password}")
+    private String adminPassword;
+    @Value("${server.admin.name}")
+    private String adminName;
     @Override
+    @Transactional(isolation = Isolation.REPEATABLE_READ)
     public void onApplicationEvent(final ApplicationReadyEvent event) {
-//        var entity = meditationRepository.findByTitle("Медитация перед сном для успокоения нервов | Медитация на ночь от стресса");
-//        entity.setVideoLink("https://storage.yandexcloud.net/auraflow/sleepMeditation.mp4");
-//        entity.setAuthor("service");
-//        meditationRepository.save(entity);
-//
-//        entity = meditationRepository.findByTitle("Медитация для успокоения нервов и продуктивности | Дневная медитация на упех | Медитация на работе");
-//        entity.setVideoLink("https://storage.yandexcloud.net/auraflow/workMeditation.mp4");
-//        entity.setAuthor("service");
-//        meditationRepository.save(entity);
-//
-//        entity = meditationRepository.findByTitle("МЕДИТАЦИЯ ДЛЯ СПОКОЙСТВИЯ и эмоционального восстановления");
-//        entity.setVideoLink("https://storage.yandexcloud.net/auraflow/relaxMeditation.mp4");
-//        entity.setAuthor("service");
-//        meditationRepository.save(entity);
-//
-//        entity = meditationRepository.findByTitle("Утренняя медитация 6 минут: женская энергия");
-//        entity.setVideoLink("https://storage.yandexcloud.net/auraflow/morningMeditation.mp4");
-//        entity.setAuthor("service");
-//        meditationRepository.save(entity);
-//
-//        entity = meditationRepository.findByTitle("ШАВАСАНА. ГЛУБОКОЕ РАССЛАБЛЕНИЕ. МЕДИТАЦИЯ. 5 МИНУТ.");
-//        entity.setVideoLink("https://storage.yandexcloud.net/auraflow/shavanaMeditation.mp4");
-//        entity.setAuthor("service");
-//        meditationRepository.save(entity);
-        userRepository.save(UserEntity
-                .builder()
-                .countBreathPractiseReminderPerDay(4)
-                .email("admin@a.com")
-                .isPremium(true)
-                .name("name")
-                .role(Role.ADMIN)
-                .password(bCryptPasswordEncoder.encode("aDm12q!sdkIs"))
-                .build());
+        if (userRepository.findByEmail(adminEmail).isEmpty()) {
+            userRepository.save(UserEntity
+                    .builder()
+                    .countBreathPractiseReminderPerDay(4)
+                    .email(adminEmail)
+                    .isPremium(true)
+                    .name(adminName)
+                    .role(Role.ADMIN)
+                    .password(bCryptPasswordEncoder.encode(adminPassword))
+                    .build());
+        }
     }
 }
