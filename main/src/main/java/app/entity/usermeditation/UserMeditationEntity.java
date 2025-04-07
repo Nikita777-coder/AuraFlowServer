79c5cc8a53bdad583753d9dc179ba2b5a248bf60
Nikita777-coder <61533809+Nikita777-coder@users.noreply.github.com>
2025-04-07 13:32:34 +1000

    completed meditationAlbumService layer without testing

---
+++ main/src/main/java/app/entity/usermeditation/UserMeditationEntity.java
@@ -0,0 +1,30 @@
+package app.entity.usermeditation;
+
+import app.entity.meditation.MeditationEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "user_meditations")
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserMeditationEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @OneToOne
+    private MeditationEntity meditationFromPlatform;
+
+    @OneToMany(mappedBy = "statusName")
+    private List<StatusEntity> statuses;
+
+    @Column(name = "pause_time")
+    private double pauseTime = 0.0;
+}
