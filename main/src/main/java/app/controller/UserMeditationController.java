c11f75a9cdb33479a7c0d8c4e50df630ad4ff0d4
Nikita777-coder <61533809+Nikita777-coder@users.noreply.github.com>
2025-04-07 13:57:19 +1000

    create user-meditation controller

--- main/src/main/java/app/controller/UserMeditationController.java
+++ main/src/main/java/app/controller/UserMeditationController.java
@@ -4,45 +4,60 @@ import app.dto.meditation.GeneratedMeditation;
 import app.dto.meditation.Meditation;
 import app.dto.meditation.MeditationRequest;
 import app.dto.meditation.ModelMeditationRequest;
+import app.service.UserMeditationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/user-meditations")
+@RequiredArgsConstructor
 public class UserMeditationController {
+    private final UserMeditationService userMeditationService;
     @PostMapping("/add")
+    @ResponseBody
     public Meditation addMeditationFromCollection(@AuthenticationPrincipal UserDetails currentUser,
-                                                  @RequestBody String meditationLink) {
-        throw new RuntimeException();
-//        return meditationService.addMeditationToUser(currentUser, meditationLink);
+                                                  @RequestParam UUID meditationId) {
+        return userMeditationService.addMeditationToUser(currentUser, meditationId);
     }
-    @GetMapping
+    @GetMapping("/all")
+    @ResponseBody
     public List<Meditation> getUserMeditations(@AuthenticationPrincipal UserDetails currentUser,
                                                @RequestBody MeditationRequest meditationRequest) {
-        throw new RuntimeException();
-//        return meditationService.generateNewMeditation(
-//                currentUser,
-//                meditationRequest
-//        );
+        return userMeditationService.getUserAll(
+                currentUser,
+                meditationRequest
+        );
     }
 
     @PostMapping("/generate")
+    @ResponseBody
     public GeneratedMeditation generateNewMeditation(@AuthenticationPrincipal UserDetails currentUser,
                                                      @RequestBody ModelMeditationRequest modelMeditationRequest) {
-        throw new RuntimeException();
-//        return meditationService.generateNewMeditation(
-//                currentUser,
-//                modelMeditationRequest
-//        );
+        return userMeditationService.generateNewMeditation(
+                currentUser,
+                modelMeditationRequest
+        );
     }
 
     @GetMapping("/specially-selected")
+    @ResponseBody
     public List<Meditation> getRecommendedMeditations(@AuthenticationPrincipal UserDetails currentUser) {
-        throw new RuntimeException();
-
-//        return meditationService.getRecommended(currentUser);
+        return userMeditationService.getRecommended(currentUser);
+    }
+    @PatchMapping
+    @ResponseBody
+    public Meditation update(@AuthenticationPrincipal UserDetails currentUser,
+                             @RequestBody MeditationRequest updateMeditationData) {
+        return userMeditationService.update(currentUser, updateMeditationData);
+    }
+    @DeleteMapping
+    public void delete(@AuthenticationPrincipal UserDetails currentUser,
+                       @RequestParam UUID id) {
+        userMeditationService.delete(currentUser, id);
     }
 }
