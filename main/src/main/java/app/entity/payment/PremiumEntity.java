4cd93b854993d15ac4891cfaa7da0090c3abea76
Nikita777-coder <61533809+Nikita777-coder@users.noreply.github.com>
2025-05-14 16:54:45 +0300

    add premium functions

--- main/src/main/java/app/entity/payment/PremiumEntity.java
+++ main/src/main/java/app/entity/payment/PremiumEntity.java
@@ -1,11 +1,7 @@
 package app.entity.payment;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import app.entity.UserEntity;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -23,8 +19,8 @@ public class PremiumEntity {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(name = "buyer_email")
-    private String userEmail;
+    @ManyToOne(targetEntity = UserEntity.class)
+    private UserEntity userEntity;
 
     @Column(name = "transaction_status")
     private TransactionStatus transactionStatus;
