3b88d8a0b0d4c61226ab58c1024bec000dc51fa5
Nikita777-coder <61533809+Nikita777-coder@users.noreply.github.com>
2025-05-14 17:32:24 +0300

    full realized not tested premium layer

--- main/src/main/java/app/mapper/PremiumMapper.java
+++ main/src/main/java/app/mapper/PremiumMapper.java
@@ -1,12 +1,15 @@
 package app.mapper;
 
 import app.dto.premium.PremiumData;
+import app.dto.premium.PremiumIntegrationServiceResponse;
 import app.entity.payment.PremiumEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface PremiumMapper {
     List<PremiumData> premiumEntitiesToPremiumDatas(List<PremiumEntity> premiumEntities);
+    PremiumEntity premiumIntegrationServiceResponseToPremiumEntity(PremiumIntegrationServiceResponse premiumIntegrationServiceResponse);
 }
--- main/src/main/java/app/service/PremiumService.java
+++ main/src/main/java/app/service/PremiumService.java
@@ -2,20 +2,32 @@ package app.service;
 
 import app.dto.payment.PaymentNotification;
 import app.dto.premium.PremiumData;
+import app.dto.premium.PremiumIntegrationServiceResponse;
+import app.entity.UserEntity;
+import app.entity.payment.PremiumEntity;
 import app.entity.payment.TransactionStatus;
 import app.mapper.PremiumMapper;
 import app.repository.PremiumRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class PremiumService {
     private final PremiumRepository premiumRepository;
     private final PremiumMapper premiumMapper;
+    private final WebClientRestService webClientRestService;
+    private final UserService userService;
+
+    @Value("${server.integration.payment-service-path}")
+    private String paymentServicePath;
+    @Value("${server.integration.base-url}")
+    private String integrationBaseUrl;
     public Boolean hasPremium(UserDetails userDetails) {
         return !premiumRepository.getCurrentPremium(
              userDetails.getUsername(),
@@ -23,7 +35,7 @@ public class PremiumService {
         ).isEmpty();
     }
     public List<PremiumData> getHistoryOfPremiums(UserDetails userDetails) {
-        return premiumMapper.premiumEntitiesToPremiumDatas(premiumRepository.getPremiumEntitiesByUserEmail(
+        return premiumMapper.premiumEntitiesToPremiumDatas(premiumRepository.getPremiumEntitiesByUserEntity_Email(
                 userDetails.getUsername()
         ));
     }
@@ -35,5 +47,23 @@ public class PremiumService {
             p.setTransactionStatus(TransactionStatus.valueOf(paymentNotification.getObject().getStatus()));
             premiumRepository.save(p);
         }
+    }
+    public UUID buyPremium(UserDetails userDetails) {
+        var ans = webClientRestService.post(
+                integrationBaseUrl,
+                paymentServicePath,
+                PremiumIntegrationServiceResponse.class
+        );
+
+        UserEntity userEntity = userService.getUserByEmail(userDetails.getUsername());
+        PremiumEntity premiumEntity = premiumMapper.premiumIntegrationServiceResponseToPremiumEntity(ans);
+        premiumEntity.setUserEntity(userEntity);
+
+        return premiumRepository.save(premiumEntity).getId();
+    }
+    public TransactionStatus getTransactionStatus(UserDetails userDetails, UUID paymentId) {
+        return premiumRepository.getPremiumEntityByIdAndUserEntity_Email(paymentId, userDetails.getUsername())
+                .orElseThrow(() -> new IllegalArgumentException("not found"))
+                .getTransactionStatus();
     }
 }
