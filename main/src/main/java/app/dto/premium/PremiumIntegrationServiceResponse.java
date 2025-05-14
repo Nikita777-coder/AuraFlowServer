8b73d47449fca907df3858593b4a09dc3f9fec09
Nikita777-coder <61533809+Nikita777-coder@users.noreply.github.com>
2025-05-14 23:01:51 +0300

    full realized not tested premium layer

--- main/src/main/java/app/dto/premium/PremiumIntegrationServiceResponse.java
+++ main/src/main/java/app/dto/premium/PremiumIntegrationServiceResponse.java
@@ -1,4 +1,18 @@
 package app.dto.premium;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
 public class PremiumIntegrationServiceResponse {
+    private UUID id;
+
+    @JsonProperty("confirmation.confirmation_url")
+    private String paymentUrl;
+
+    private boolean test;
 }
